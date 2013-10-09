541a7358617b21836606b2cc8f39bcb7db7cac30
Petr Blahos <petrblahos@gmail.com>
2013-10-09 20:23:46 +0200

    Adds a per-user salt into the ticket generation.
    Based on step03, adding a salt, which is re-generated on
    each password change, therefore effectively logging the user
    out from other browsers when the password has changed.

--- step04.py
+++ step04.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
+from collections import defaultdict
 import datetime
-import os.path
+import hashlib
+import logging
+import random
+import time
 
 from mako.template import Template
 from pyramid.response import Response
-from pyramid.security import remember, forget, Authenticated, Everyone
 from pyramid.view import view_config
 from pyramid.config import Configurator
 from pyramid.httpexceptions import HTTPBadRequest
@@ -19,6 +22,7 @@ main_template = Template("""<html><head>
 <a href="${ request.route_url('login', _query={'userid': 'john'}) }">Login as john</a><br/>
 <a href="${ request.route_url('login', _query={'userid': 'bob'}) }">Login as bob</a><br/>
 <hr/>
+<a href="${ request.route_url('passwd') }">Change Password</a><br/>
 <a href="${ request.route_url('logout') }">Logout</a><br/>
 <hr/>
 ${ msg }
@@ -26,18 +30,60 @@ ${ msg }
 User ID: ${ identity if not identity is None else "--not-set--" }
 </body></html>""")
 
+def calculate_digest(secret, userid, usersalt, timestamp, ip):
+    m = hashlib.sha1()
+    m.update(str(secret))
+    m.update(str(userid))
+    m.update(str(usersalt))
+    m.update(str(timestamp))
+    m.update(str(ip))
+    return m.hexdigest()
+
 class View(object):
+    SECRET = "verysecretstring"
+    USER_SALT = defaultdict(lambda : str(random.randint(1000000, 9999999)))
+    def _encode_cookie(self, userid):
+        # the most basic version:
+        #return str(userid)
+        ip = ""
+        ts = int(time.time())
+        digest = calculate_digest(self.SECRET, userid, self.USER_SALT[userid], ts, ip)
+        return "%s-%s-%s" % (digest, ts, userid)
+
+    def _decode_cookie(self):
+        # the most basic version:
+        #return self.request.cookies.get("userid", None)
+        cookie = self.request.cookies.get("userid", None)
+        if not cookie:
+            return None
+        # try to extract a userid and timestamp from the cookie
+        try:
+            (digest, ts, userid) = cookie.split("-", 2)
+            logging.info("cookie splitted up:%s-%s-%s" % (digest, ts, userid, ))
+        except:
+            logging.error("BAD COOKIE FORMAT:%s|" % cookie)
+            response = HTTPBadRequest()
+            response.delete_cookie("userid")
+            raise response
+        ip = ""
+        d2 = calculate_digest(self.SECRET, userid, self.USER_SALT[userid], ts, ip)
+        if d2==digest:
+            return userid
+        logging.error("bad digest")
+        response = HTTPBadRequest()
+        response.delete_cookie("userid")
+        raise response
+
     def __init__(self, request):
         self.request = request
+        self.identity = self._decode_cookie()
 
-    def response(self, msg, headers=[]):
+    def response(self, msg):
         return Response(main_template.render(
                 request=self.request,
                 msg=msg,
-                identity="IIII",
-            ),
-            headers=headers
-        )
+                identity=self.identity
+            ))
 
     @view_config(route_name="root", )
     def root_view(self):
@@ -46,60 +92,33 @@ class View(object):
     @view_config(route_name="login", )
     def login_view(self):
         userid = self.request.params.get("userid")
-        return self.response(["LOGGED IN", userid, ], headers=remember(self.request, userid))
+        response = self.response(["LOGGED IN", userid ])
+        response.set_cookie("userid", self._encode_cookie(userid), httponly=1)    # session - cleared with browser exit
+        return response
 
     @view_config(route_name="logout", )
     def logout_view(self):
-        self.request.response.delete_cookie("userid")
-        return self.response(["LOGGED OUT", ], headers=forget(self.request, userid))
+        response = self.response(["LOGGED OUT" ])
+        response.delete_cookie("userid")
+        return response
 
-class DumbAuthenticationPolicy(object):
-    def unauthenticated_userid(self, request):
-        userid = request.cookies.get('userid')
-        return userid
-
-    authenticated_userid = unauthenticated_userid
-
-    def effective_principals(self, request):
-        principals = [Everyone]
-        userid = self.unauthenticated_userid(request)
-        if userid is not None:
-            principals.append(Authenticated)
-            principals.append(userid)
-        return principals
-
-    def remember(self, request, principal, **kw):
-        return [
-            ('Set-Cookie',
-             'userid=%s' % str(principal))
-            ]
-
-    def forget(self, request):
-        return [
-            ('Set-Cookie',
-             'userid=deleted; Expires=Thu, 01-Jan-1970 00:00:01 GMT')
-            ]
-
-class DumbAuthorizationPolicy(object):
-    def permits(self, context, principals,
-                permission):
-        if permission == 'delete':
-            return Authenticated in principals
-        return False
+    @view_config(route_name="passwd")
+    def passwd_view(self):
+        # When the user changes the password, we generate a new random string
+        # and "store it into the database with the password".
+        del self.USER_SALT[self.identity]
+        # Then we re-generate the cookie, otherwise we will be logged out.
+        response = self.response(["PASSWORD CHANGED", self.identity ])
+        response.set_cookie("userid", self._encode_cookie(self.identity), httponly=1)    # session - cleared with browser exit
+        return response
 
 if __name__ == '__main__':
-    authn_policy = DumbAuthenticationPolicy()
-    authz_policy = DumbAuthorizationPolicy()
-    config = Configurator(
-        authentication_policy=authn_policy,
-        authorization_policy=authz_policy
-        )
+    config = Configurator()
     config.add_route('root', '')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
+    config.add_route('passwd', '/password')
     config.scan()
     app = config.make_wsgi_app()
     serve(app)
-
-
 
